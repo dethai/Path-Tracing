5fea61c69299b186aa23d00c62876077be50b5ed
Vivienne Yao <vivienneyao@VI-MacBook-Pro.local>
2026-05-18 17:12:06 +0200

    lambertian diffusion added in camera and gamma correction added in color.h

--- headerfile/camera.h
+++ headerfile/camera.h
@@ -12,6 +12,7 @@ class camera {
     double aspect_ratio = 1.0;  // Ratio of image width over height
     int    image_width  = 100;  // Rendered image width in pixel count
     int    samples_per_pixel = 10;   // Count of random samples for each pixel
+    int    max_depth         = 10; // max depth of the recursive function (avoid too many bounces)
 
     void render(const hittable& world) {
         initialize();
@@ -21,12 +22,12 @@ class camera {
         for (int j = 0; j < image_height; j++) {
             std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
             for (int i = 0; i < image_width; i++) {
-                auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-                auto ray_direction = pixel_center - center;
-                ray r(center, ray_direction);
-
-                color pixel_color = ray_color(r, world);
-                write_color(std::cout, pixel_color);
+                color pixel_color(0,0,0);
+                for (int sample = 0; sample < samples_per_pixel; sample++) { // we do multiple rays around the pixel and average the result 
+                    ray r = get_ray(i, j);
+                    pixel_color += ray_color(r, max_depth, world);
+                }
+                write_color(std::cout, pixel_samples_scale * pixel_color);
             }
         }
 
@@ -85,12 +86,15 @@ class camera {
         return vec3(random_double() - 0.5, random_double() - 0.5, 0);
     }
 
-    color ray_color(const ray& r, const hittable& world) const {
+    color ray_color(const ray& r, int depth, const hittable& world) const {
+        // If we've exceeded the ray bounce limit, no more light is gathered. prevent stack overflow
+        if (depth <= 0)
+            return color(0,0,0);
         hit_record rec;
-
-        if (world.hit(r, interval(0, infinity), rec)) {
+        //we choose the precision of the origin of the bounce it should be above the surface
+        if (world.hit(r, interval(0.001, infinity), rec)) {
             vec3 direction = random_on_hemisphere(rec.normal);
-            return 0.5 * ray_color(ray(rec.p, direction), world);
+            return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
